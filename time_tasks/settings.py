73a0eeaab7362394470b2cfc23eb940c822fa8ae
skccpa <admin@technicallycpa.com>
2021-03-18 05:11:44 -0500

    test update comment settings to push through to heroku

--- time_tasks/settings.py
+++ time_tasks/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+#static
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
