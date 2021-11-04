9f718f6200706f1ed7cc46782a0c6e2bf8191580
cpatechnically <admin@technicallycpa.com>
2021-11-04 14:54:49 -0500

    updates to templates

--- time_tasks/settings/__init__.py
+++ time_tasks/settings/__init__.py
@@ -1,5 +1,22 @@
-#from .base import *
+import os
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+LINODE_BUCKET = 'demo-celarydjango'
+LINODE_BUCKET_REGION = 'us-southeast-1'
+LINODE_BUCKET_ACCESS_KEY = os.environ.get("LINODE_BUCKET_ACCESS_KEY")
+LINODE_BUCKET_SECRET_KEY = os.environ.get("LINODE_BUCKET_SECRET_KEY")
+
+AWS_S3_ENDPOINT_URL = f'https://{LINODE_BUCKET_REGION}.linodeobjects.com'
+AWS_ACCESS_KEY_ID = LINODE_BUCKET_ACCESS_KEY
+AWS_SECRET_ACCESS_KEY = LINODE_BUCKET_SECRET_KEY
+AWS_S3_REGION_NAME = LINODE_BUCKET_REGION
+AWS_S3_USE_SSL = True
+AWS_STORAGE_BUCKET_NAME = LINODE_BUCKET
+
+#from .base import *
+print(f"\nAWS_ACCESS_KEY_ID {AWS_ACCESS_KEY_ID} \n")
 from .production import *
 
 #from .local import *
--- time_tasks/settings/production.py
+++ time_tasks/settings/production.py
@@ -141,7 +141,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'my_static'),
 ]
 #static
-from time_tasks.static_srv.production import *
+#from time_tasks.static_srv.production import *
 
 
 if DEBUG:
@@ -160,3 +160,20 @@ SECURE_FRAME_DENY               = False
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+LINODE_BUCKET = 'demo-celarydjango'
+LINODE_BUCKET_REGION = 'us-southeast-1'
+LINODE_BUCKET_ACCESS_KEY = os.environ.get("LINODE_BUCKET_ACCESS_KEY")
+LINODE_BUCKET_SECRET_KEY = os.environ.get("LINODE_BUCKET_SECRET_KEY")
+
+AWS_S3_ENDPOINT_URL = f'https://{LINODE_BUCKET_REGION}.linodeobjects.com'
+AWS_ACCESS_KEY_ID = LINODE_BUCKET_ACCESS_KEY
+AWS_SECRET_ACCESS_KEY = LINODE_BUCKET_SECRET_KEY
+AWS_S3_REGION_NAME = LINODE_BUCKET_REGION
+AWS_S3_USE_SSL = True
+AWS_STORAGE_BUCKET_NAME = LINODE_BUCKET
